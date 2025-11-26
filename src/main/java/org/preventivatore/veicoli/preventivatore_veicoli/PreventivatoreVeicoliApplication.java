c94cb18ca2b3f9b22672b26275d0ccb1f1041b09
ruzica-glavas <ruzi99@hotmail.it>
2025-11-26 16:46:16 +0100

    first commit

---
+++ src/main/java/org/preventivatore/veicoli/preventivatore_veicoli/PreventivatoreVeicoliApplication.java
@@ -0,0 +1,13 @@
+package org.preventivatore.veicoli.preventivatore_veicoli;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PreventivatoreVeicoliApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PreventivatoreVeicoliApplication.class, args);
+	}
+
+}
---
+++ src/test/java/org/preventivatore/veicoli/preventivatore_veicoli/PreventivatoreVeicoliApplicationTests.java
@@ -0,0 +1,13 @@
+package org.preventivatore.veicoli.preventivatore_veicoli;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PreventivatoreVeicoliApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
